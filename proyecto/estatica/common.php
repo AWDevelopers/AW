e6c04c36c04f67a8a6e17162c5c1976881e09a3c
juan jose montiel cano <juan jose montiel cano>
2016-04-04 19:05:13 +0200

    conocenos acabado y archivo php

---
+++ proyecto/estatica/common.php
@@ -0,0 +1,15 @@
+﻿ <?php
+ 	echo '<div id="cabecera">';
+			echo '<div class="avatar"> <IMG SRC="img/usuarioSF.png" WIDTH=120 HEIGHT=120 ALT="Avatar usuario"> </div>';
+			echo '<div class="sesion"> <IMG SRC="img/power.png" WIDTH=60 HEIGHT=60 ALT="Avatar usuario"> </div>';		
+ 	echo '</div>';
+	echo '<div id="sidebar-left">';
+			echo '<ul>';
+				echo '<li><a href="index.html">Inicio</a></li>';
+            	echo '<li><a href="#">Donaciones</a></li>';
+            	echo '<li><a href="#">Voluntarios</a></li>';
+            	echo '<li><a href="conocenos.html">Conocenos</a></li>';
+            	echo '<li><a href="#">Tienda</a></li>';
+        	echo '</ul>';
+	echo '</div>';
+ ?>
