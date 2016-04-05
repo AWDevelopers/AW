cc322f695efd3b53280e343ca7d7d4c23032f013
juan jose montiel cano <juan jose montiel cano>
2016-04-05 17:41:39 +0200

    añadida pagina inicial de noticias

--- proyecto/estatica/index.php
+++ proyecto/estatica/index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<div id='contenedor'>
-	<?php require 'common.php'; ?>
+		<?php require 'common.php'; ?>
 		<!--<div id="cabecera">
 			<div class="avatar">
 				<IMG SRC="img/usuarioSF.png" WIDTH=120 HEIGHT=120 ALT="Avatar usuario">
@@ -26,7 +26,39 @@
 		</div> 
 		-->
 		<div id="contenido">
-			
+			<div id="noticias">
+				<h1>Noticias</h1>
+				<div class="principal">
+					<img src="img/imgPrincipal.png">
+					<h2>Titulo principal</h2>
+					<p>Texto</p>
+				</div>
+				<div class="secundaria">
+					<img src="img/imgSecundaria.jpeg">
+					<h3>Titulo secundaria</h3>
+					<p>Texto</p>
+				</div>
+				<div class="secundaria">
+					<img src="img/imgSecundaria2.jpeg">
+					<h3>Titulo secundaria</h3>
+					<p>Lorem ipsum</p>
+				</div>
+				<div class="terciaria">
+					<img src="img/imgTerciaria.jpeg">
+					<h4>Titulo terciaria</h4>
+					<p>Texto</p>
+				</div>
+				<div class="terciaria">
+					<img src="img/imgTerciaria2.jpeg">
+					<h4>Titulo terciaria</h4>
+					<p>Texto</p>
+				</div>
+				<div class="terciaria">
+					<img src="img/imgTerciaria3.jpeg">
+					<h4>Titulo terciaria</h4>
+					<p>Texto</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </body>
