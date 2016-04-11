af00c0414fe68c295472584d44d2d9ea71f94335
juan jose montiel cano <juan jose montiel cano>
2016-04-11 11:17:32 +0200

    proyecto

---
+++ proyecto/estatica/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div id='contenedor'>
+		<?php require 'common.php'; ?>
+		<!--<div id="cabecera">
+			<div class="avatar">
+				<IMG SRC="img/usuarioSF.png" WIDTH=120 HEIGHT=120 ALT="Avatar usuario">
+			</div>
+			<div class="sesion">
+				<IMG SRC="img/power.png" WIDTH=60 HEIGHT=60 ALT="Avatar usuario">
+			</div>
+		</div>
+		<div id="sidebar-left">
+			<ul>
+				<li><a href="index.html">Inicio</a></li>
+            	<li><a href="#">Donaciones</a></li>
+            	<li><a href="#">Voluntarios</a></li>
+            	<li><a href="conocenos.html">Conocenos</a></li>
+            	<li><a href="#">Tienda</a></li>
+        	</ul>
+		</div> 
+		-->
+		<div id="contenido">
+			<div id="noticias">
+				<h1>Noticias</h1>
+				<div class="principal">
+					<img src="img/imgPrincipal.png">
+					<h2>Titulo principal</h2>
+					<p>Texto</p>
+				</div>
+				<div class="bloque_secundaria">
+					<div class="secundaria">
+						<img src="img/imgSecundaria.jpeg">
+						<h3>Titulo secundaria</h3>
+						<p>Texto</p>
+					</div>
+					<div class="secundaria">
+						<img src="img/imgSecundaria2.jpeg">
+						<h3>Titulo secundaria</h3>
+						<p>Texto</p>
+					</div>
+				</div>
+				<div class="bloque_terciaria">
+					<div class="terciaria">
+						<img src="img/imgTerciaria.jpeg">
+						<h4>Titulo terciaria</h4>
+						<p>Texto</p>
+					</div>
+					<div class="terciaria">
+						<img src="img/imgTerciaria2.jpeg">
+						<h4>Titulo terciaria</h4>
+						<p>Texto</p>
+					</div>
+					<div class="terciaria">
+						<img src="img/imgTerciaria3.jpeg">
+						<h4>Titulo terciaria</h4>
+						<p>Texto</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ proyecto/estatica/vistaProyecto.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proyectos ONGS</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" type="text/css" href="proyectosONG.css">
+</head>
+<body>
+	<div id='contenedor'>
+		<!--Aqui va el menu y la cabecera que es comun-->
+		<?php require 'common.php'; ?>
+		
+		<!--Aqui va el contenido-->
+		<div id="contenido">
+			<div id="proyectoONG">
+				<h1>Nombre ONG </h1>
+				<h2> Nombre proyecto </h2>
+				<p> Aqui va la descripcion del proyecto de la ONG correspondiente </p>	
+				<!--Aqui van las fechas del proyecto-->
+				<div class="proyectoFechas">Fechas: 12/05/2016</div>
+				
+				<!--Aqui van los voluntarios necesarios -->
+				<div class="proyectoVoluntario">Voluntarios necesarios: 50 </div>
+			
+				<!--Aqui va el botón Apuntame-->
+				<div class="boton"><button> APUNTAME </button></div>
+				
+			</div>
+			
+		</div>
+			
+
+		</div>
+	</div>
+</body>
+</html>
