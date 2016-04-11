82fb2c5dc540e48a82a4ccfd3c751a6bb15538fd
Nerea <negomez@ucm.es>
2016-04-11 12:57:29 +0200

    Added files via upload

--- proyecto/estatica/voluntarios.php
+++ proyecto/estatica/voluntarios.php
@@ -2,8 +2,9 @@
 <html>
 <head>
 	<title>Voluntarios ONGS</title>
-	<link rel="stylesheet" type="text/css" href="estilos.css" >
+	<link rel="stylesheet" type="text/css" href="estilos.css">
 	<link rel="stylesheet" type="text/css" href="voluntarios.css" >
+
 </head>
 <body>
 	<div id='contenedor'>
@@ -12,16 +13,33 @@
 		
 		<!--Aqui va el contenido-->
 		<div id="contenido">
-			<div class="contenidoVoluntario">
-				<div class="imagenUsuario"> <img src="img/usuarioSF.png"></div>
-				<div class="nombreUsuario"> NOMBRE DEL USUARIO </div>
-			</div>
-			
-			<div class="contenidoVoluntario">
-				<div class="imagenUsuario"> <img src="img/usuarioSF.png"></div>
-				<div class="nombreUsuario"> NOMBRE DEL USUARIO </div>
-			</div>
-			
+
+				<div class="cajaTitulos"> Nombres </div>
+				<div class="cajaTitulos"> Horas disponibles </div>
+				<div class="contenidoVoluntario">
+					<div class="imagenUsuario"> <img src="img/usuarioSF.png"></div>
+					<div class="nombreUsuario"> NOMBRE DEL USUARIO 1 </div>
+					<div class="nombreUsuario"> 10 h </div>
+					<div class="boton"> <button> Apuntar </button> </div>
+				</div>
+				
+				<div class="contenidoVoluntario">
+					<div class="imagenUsuario"> <img src="img/usuarioSF.png"></div>
+					<div class="nombreUsuario"> NOMBRE DEL USUARIO 2</div>
+					<div class="nombreUsuario"> 10 h </div>
+					<div class="boton"> <button> Apuntar </button> </div>
+				</div>
+				
+				<div class="contenidoVoluntario">
+					<div class="imagenUsuario"> <img src="img/usuarioSF.png"></div>
+					<div class="nombreUsuario"> NOMBRE DEL USUARIO 3</div>
+					<div class="nombreUsuario"> 10 h </div>
+					<div class="boton"> <button> Apuntar </button> </div>
+				</div>
+				
+				
+				
+				
 		</div>
 			
 
