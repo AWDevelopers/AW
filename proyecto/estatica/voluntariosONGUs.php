e1014408daf9ebd302adc14c2aaedf75069dc468
SilviaLenFer <sillendi@ucm.es>
2016-04-11 11:32:33 +0200

    Added files via upload

---
+++ proyecto/estatica/voluntariosONGUs.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ONGS</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" type="text/css" href="estilosSilviaVolunt.css">
+</head>
+<body>
+	<div id='contenedor'>
+		<!--Aqui va el menu y la cabecera que es comun-->
+		<?php require 'common.php'; ?>
+		
+		<!--Aqui va el contenido-->
+		<div id="contenido">
+			<div class="contenidoVoluntarios">
+				<div class="textoVoluntarios">
+					<h2> Nombre ONG 1 </h2>
+					<p> Aqui va la descripción del proyecto </p>
+				</div>
+				<div class="botonVoluntarios">
+					<button type="button">APUNTATE</button>
+				</div>
+				
+			</div>
+            
+			<div class="contenidoVoluntarios">
+				<div class="textoVoluntarios">
+					<h2> Nombre ONG 2 </h2>
+					<p> Aqui va la descripción del proyecto </p>
+				</div>
+				<div class="botonVoluntarios">
+					<button type="button">APUNTATE</button>
+				</div>
+				
+			</div>
+			
+			<div class="contenidoVoluntarios">
+				<div class="textoVoluntarios">
+					<h2> Nombre ONG 3 </h2>
+					<p> Aqui va la descripción del proyecto </p>
+				</div>
+				<div class="botonVoluntarios">
+					<button type="button">APUNTATE</button>
+				</div>
+				
+			</div>
+			
+			<div class="contenidoVoluntarios">
+				<div class="textoVoluntarios">
+					<h2> Nombre ONG 4 </h2>
+					<p> Aqui va la descripción del proyecto </p>
+				</div>
+				<div class="botonVoluntarios">
+					<button type="button">APUNTATE</button>
+				</div>
+				
+			</div>
+			
+
+		</div>
+	</div>
+</body>
+</html>
