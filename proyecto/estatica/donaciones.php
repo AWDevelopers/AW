94e601619ecf1b854b0076ae5a6f6a7d86311383
juan jose montiel cano <juan jose montiel cano>
2016-04-12 10:17:35 +0200

    merge changes

--- proyecto/estatica/donaciones.php
+++ proyecto/estatica/donaciones.php
@@ -3,22 +3,11 @@
 <head>
 	<title>Pagina con estilos</title>
 	<link rel="stylesheet" type="text/css" href="estilos.css"/>
+	<link rel="stylesheet" type="text/css" href="donacion.css"/>
 </head>
 <body>
 	<div id='contenedor'>
-		<div id="cabecera">
-			<div class="avatar">Avatar con foto</div>
-			<div class="sesion"> cierre de sesion</div>
-			
-		</div>
-		<div id="sidebar-left">
-			<ul>
-            <li><a href="#">Donaciones</a></li>
-            <li><a href="#">Voluntarios</a></li>
-            <li><a href="#">Conocenos</a></li>
-            <li><a href="#">Tienda</a></li>
-        </ul>
-		</div>
+		<?php require 'common.php'; ?>
 		<div id="contenido">
 			<div id = 'fundacion'>
 				<h1 align="center">Nombre ONG/Fundacion</h1>
@@ -31,33 +20,26 @@
 				nananananananananananannannanananananananannananananannananananannananananananananananananananananan
 				nananananananananananannannanananananananannananananannananananannananananananananananananananananan
 				nananananananananananannannanananananananannananananannananananannananananananananananananananananan</p>
-				<p>Nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan</p>
-				<p>Nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan</p>
-				<p>Nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan</p>
-				<p>Nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan</p>
-				<p>Nananananananananananannannanananananananannananananannananananannananananananananananananananananan
-				nananananananananananannannanananananananannananananannananananannananananananananananananananananan</p>
 				</center>
 			</div>
 			<div id = "recaudado">
-					<align = "left">Recaudacion: 89.536 euros</align>
-					<align = "center">Meta: 100.000 euros</align>
-					<align = "right"> Cantidad: 355 euros</align>
-				</div>
-					<div id = "barrainformativa">
-						<p align= "center"><img src="img/barraestado.png"/></p>
-					</div>
-				<div>
-					<center><input type= "submit" value = "Donar"></center>
-				</div>
+				<center>
+					<div id= "recaudacion">Recaudacion: 89.536 euros</div>
+					<div id= "meta">Meta: 100.000 euros</div>
+					<div id= "cantidad"> Cantidad: 355 euros</div>
+				</center>
+			</div>
+			<div class="progress">
+			  <div class="progress-bar" role="progressbar" aria-valuenow="60"
+			       aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+			    <span class="sr-only">60% completado</span>
+			  </div>
+			</div>
+			<div id ="boton">
+				<center>
+				<p><input type= "submit" value = "Donar"></p>
+				</center>
+			</div>
 		</div>
 	</div>
 </body>
