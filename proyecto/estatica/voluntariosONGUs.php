63b2f77829e147663dfab4ca491e8d394487d65b
juan jose montiel cano <juan jose montiel cano>
2016-04-14 17:55:46 +0200

    introducidos botones

--- proyecto/estatica/voluntariosONGUs.php
+++ proyecto/estatica/voluntariosONGUs.php
@@ -3,8 +3,9 @@
 <html>
 <head>
 	<title>ONGS</title>
-	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" type="text/css" href="estilos.css">	
 	<link rel="stylesheet" type="text/css" href="estilosSilviaVolunt.css">
+
 </head>
 <body>
 	<div id='contenedor'>
@@ -17,55 +18,33 @@
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 1 </h2>
 					<p> Aqui va la descripción del proyecto </p>
+					<button type="button"> Apuntate </button>
 				</div>
-				<div class="botonVoluntarios">
-					<form action="vistaProyecto.php">
-					<input type="submit" value="APUNTATE">
-					</form>
-				</div>
-				
 			</div>
             
 			<div class="contenidoVoluntarios">
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 2 </h2>
 					<p> Aqui va la descripción del proyecto </p>
+					<button type="button"> Apuntate </button>
 				</div>
-				<div class="botonVoluntarios">
-					<form action="vistaProyecto.php">
-					<input type="submit" value="APUNTATE">
-					</form>
-				</div>
-				
 			</div>
 			
 			<div class="contenidoVoluntarios">
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 3 </h2>
 					<p> Aqui va la descripción del proyecto </p>
+					<button type="button"> Apuntate </button>
 				</div>
-				<div class="botonVoluntarios">
-					<form action="vistaProyecto.php">
-					<input type="submit" value="APUNTATE">
-					</form>
-				</div>
-				
 			</div>
 			
 			<div class="contenidoVoluntarios">
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 4 </h2>
 					<p> Aqui va la descripción del proyecto </p>
+					<button type="button"> Apuntate </button>
 				</div>
-				<div class="botonVoluntarios">
-					<form action="vistaProyecto.php">
-					<input type="submit" value="APUNTATE">
-					</form>
-				</div>
-				
 			</div>
-			
-
 		</div>
 	</div>
 </body>
