c73550167e354d7d1b80bba7d1f3b3e4fef7e47f
mirlop01 <mirlop01@ucm.es>
2016-04-15 16:36:44 +0200

    Update voluntariosONGUs.php

--- proyecto/estatica/voluntariosONGUs.php
+++ proyecto/estatica/voluntariosONGUs.php
@@ -18,7 +18,7 @@
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 1 </h2>
 					<p> Aqui va la descripción del proyecto </p>
-					<button type="button"> Apuntate </button>
+					<button type="button" onclick ="location = 'calendarioUsuario.php'"> Apuntate </button>
 				</div>
 			</div>
             
@@ -26,7 +26,7 @@
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 2 </h2>
 					<p> Aqui va la descripción del proyecto </p>
-					<button type="button"> Apuntate </button>
+					<button type="button" onclick ="location = 'calendarioUsuario.php'"> Apuntate </button>
 				</div>
 			</div>
 			
@@ -34,7 +34,7 @@
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 3 </h2>
 					<p> Aqui va la descripción del proyecto </p>
-					<button type="button"> Apuntate </button>
+					<button type="button" onclick ="location = 'calendarioUsuario.php'"> Apuntate </button>
 				</div>
 			</div>
 			
@@ -42,7 +42,7 @@
 				<div class="textoVoluntarios">
 					<h2> Nombre ONG 4 </h2>
 					<p> Aqui va la descripción del proyecto </p>
-					<button type="button"> Apuntate </button>
+					<button type="button"onclick ="location = 'calendarioUsuario.php'"> Apuntate </button>
 				</div>
 			</div>
 		</div>
