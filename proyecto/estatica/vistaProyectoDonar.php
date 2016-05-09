c7aca81558afecdaf91e967033a66a238505d3c0
juan jose montiel cano <juan jose montiel cano>
2016-05-09 18:39:03 +0200

    recuperando proyectos de la base de datos

--- proyecto/estatica/vistaProyectoDonar.php
+++ proyecto/estatica/vistaProyectoDonar.php
@@ -13,97 +13,55 @@
 		
 		<!--Aqui va el contenido-->
 		<div id="contenido">
-			<div class="contenidoDonaciones">
-            
-				<h1> Nombre Proyecto 1 </h1>
-				<!--Aqui va una imagen-->
-				<div class="imgDonaciones">
-					<a href="donaciones.php"> <img src="img/medicos.jpg" > </a>
-					<p>Aqui va la descripción del proyecto</p>
-					<!--Aqui va el botón donar-->
-					<button type="button">DONAR</button>
-				</div>
-				
-				<!--Aqui va la barra-->
-			</div>
-            
-			
-            <div class="contenidoDonaciones">
-				
-				<h1> Nombre Proyecto 2 </h1>
-				<!--Aqui va una imagen-->
-				<div class="imgDonaciones">
-					<a href="donaciones.php">  <img src="img/panda.png"> </a>
-					<p>Aqui va la descripción del proyecto</p>
-					
-					<!--Aqui va el botón donar-->
-					<button type="button">DONAR</button>
-				</div>
 
-				<!--Aqui va la barra-->
-			</div>
+		<?php
 
-			<div class="contenidoDonaciones">
-            
-				<h1> Nombre Proyecto 1 </h1>
-				<!--Aqui va una imagen-->
-				<div class="imgDonaciones">
-					<a href="donaciones.php"> <img src="img/medicos.jpg" > </a>
-					<p>Aqui va la descripción del proyecto</p>
-					<!--Aqui va el botón donar-->
-					<button type="button">DONAR</button>
-				</div>
-				
-				<!--Aqui va la barra-->
-			</div>
+			$mysqli = new mysqli('localhost', 'root', '', 'incommong');
 
-			<div class="contenidoDonaciones">
-				
-				<h1> Nombre Proyecto 3 </h1>
-				<!--Aqui va una imagen-->
-				<div class="imgDonaciones">
-					<a href="donaciones.php">  <img src="img/panda.png"> </a>
-					<p>Aqui va la descripción del proyecto</p>
-					
-					<!--Aqui va el botón donar-->
-					<button type="button">DONAR</button>
-				</div>
+			// ¡Oh, no! Existe un error 'connect_errno', fallando así el intento de conexión
+			if ($mysqli->connect_errno) {
+			    echo "Lo sentimos, este sitio web está experimentando problemas.";
+			    echo "Error: Fallo al conectarse a MySQL debido a: \n";
+			    echo "Errno: " . $mysqli->connect_errno . "\n";
+			    echo "Error: " . $mysqli->connect_error . "\n";
+			    exit;
+			}
 
-				<!--Aqui va la barra-->
-			</div>
+			// Realizar una consulta SQL
+			$sql = "SELECT nombre, descripcionCorta, imagen FROM proyecto";
+			if (!$resultado = $mysqli->query($sql)) {
+			    // ¡Oh, no! La consulta falló. 
+			    echo "Lo sentimos, este sitio web está experimentando problemas.";
 
-			<div class="contenidoDonaciones">
-            
-				<h1> Nombre Proyecto 1 </h1>
-				<!--Aqui va una imagen-->
-				<div class="imgDonaciones">
-					<a href="donaciones.php"> <img src="img/medicos.jpg" > </a>
-					<p>Aqui va la descripción del proyecto</p>
-					<!--Aqui va el botón donar-->
-					<button type="button">DONAR</button>
-				</div>
-				
-				<!--Aqui va la barra-->
-			</div>
+			    // De nuevo, no hacer esto en un sitio público, aunque nosotros mostraremos
+			    // cómo obtener información del error
+			    echo "Error: La ejecución de la consulta falló debido a: \n";
+			    echo "Query: " . $sql . "\n";
+			    echo "Errno: " . $mysqli->errno . "\n";
+			    echo "Error: " . $mysqli->error . "\n";
+			    exit;
+			}
+			 while ($row = $resultado->fetch_assoc()) {
+			 	echo "<div class='contenidoDonaciones'>";
+			  		 echo "<h1>" . $row [ "nombre" ] . "</h1>";
+			  		 echo "<div class='imgDonaciones'>";
+			  		 	/*if($row [ "imagen" ] != null){
+			  		 		echo '<a href="donaciones.php"> <img src="'.  $row [ "imagen" ]. ' alt="" > </a>';
+			  		 	}
+			  		 	else{*/
+			  		 		echo '<a href="donaciones.php"> <img src="img/panda.png" > </a>';
+			  		 	//}
+			  		 	echo "<p>".  $row [ "descripcionCorta" ] . "</p>";
+			  		 	echo '<button type="button">DONAR</button> </div>';
+			  		echo "</div>";
+   			 }
 
-			<div class="contenidoDonaciones">
-				
-				<h1> Nombre Proyecto 3 </h1>
-				<!--Aqui va una imagen-->
-				<div class="imgDonaciones">
-					<a href="donaciones.php">  <img src="img/panda.png"> </a>
-					<p>Aqui va la descripción del proyecto</p>
-					
-					<!--Aqui va el botón donar-->
-					<button type="button">DONAR</button>
-				</div>
+			// El script automáticamente liberará el resultado y cerrará la conexión
+			// a MySQL cuando finalice, aunque aquí lo vamos a hacer nostros mismos
+			$resultado->free();
+			$mysqli->close();
 
-				<!--Aqui va la barra-->
-			</div>
-
-			
-			
-
+		?>
 		</div>
 	</div>
 </body>
