bc35e27177362dbc2ebdbbb3d1a6e7e44dd7d4be
mirlop01 <mirlop01@ucm.es>
2016-05-16 15:54:03 +0200

    Update tienda.php

--- proyecto/estatica/tienda.php
+++ proyecto/estatica/tienda.php
@@ -2,7 +2,10 @@
 	//Producto
 	$query_SacarProductos = 'SELECT * FROM producto';
 	$SacarProductos = mysqli_query($connection, $query_SacarProductos);
-	$row_SacarProductos = mysqli_fetch_assoc($SacarProductos);
+	if(mysqli_num_rows($SacarProductos) > 0){
+		$row_SacarProductos = mysqli_fetch_assoc($SacarProductos);
+	}
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,29 +32,36 @@
 				<button>Ventas</button>
 			</div>
 
-			<?php do {
-				$nombreOngDelProducto = $row_SacarProductos['CIFOng'];
-				$query_SacarCif = "SELECT nombre FROM ong WHERE  CIF = $nombreOngDelProducto";
-				$SacarCif = mysqli_query($connection, $query_SacarCif);
-				$row_SacarCif = mysqli_fetch_assoc($SacarCif);
+			<?php 
+				if(mysqli_num_rows($SacarProductos) > 0){
+					do {
+						$nombreOngDelProducto = $row_SacarProductos['CIFOng'];
+						$query_SacarCif = "SELECT nombre FROM ong WHERE  CIF = $nombreOngDelProducto";
+						$SacarCif = mysqli_query($connection, $query_SacarCif);
+						$row_SacarCif = mysqli_fetch_assoc($SacarCif);
 			?>
-				<div class="producto"> 
-					<h3><?php echo $row_SacarProductos['nombre'] ?></h3>
-					<a href="producto/<?php echo $row_SacarProductos['idProducto'] ?>"><img src="img/default-image.jpg"/><a>
-					<h3><?php echo $row_SacarCif['nombre'] ?></h3>
-					<h3 class="precio"> Precio: <?php echo $row_SacarProductos['precio'] ?> €</h3>
-					<form>
-						<select>
-							<?php for ($i=1; $i <= $row_SacarProductos['stock'] ; $i++) { ?>
-								<option <?php if ($i==1) echo 'selected'?>> <?php echo $i ?>
-							<?php } ?>
-						</select>
-						<input type="submit" value="Comprar">
-					</form>
-				</div>
-			<?php } while ($row_SacarProductos = mysqli_fetch_assoc($SacarProductos)); ?>
+						<div class="producto"> 
+							<h3><?php echo $row_SacarProductos['nombre'] ?></h3>
+							<a href="producto/<?php echo $row_SacarProductos['idProducto'] ?>"><img src="img/default-image.jpg"/><a>
+							<h3><?php echo $row_SacarCif['nombre'] ?></h3>
+							<h3 class="precio"> Precio: <?php echo $row_SacarProductos['precio'] ?> €</h3>
+							<form>
+								<select>
+									<?php for ($i=1; $i <= $row_SacarProductos['stock'] ; $i++) { ?>
+										<option <?php if ($i==1) echo 'selected'?>> <?php echo $i ?>
+									<?php } ?>
+								</select>
+								<input type="submit" value="Comprar">
+							</form>
+						</div>
+			<?php 	} while ($row_SacarProductos = mysqli_fetch_assoc($SacarProductos)); 
+
+					mysqli_free_result($SacarCif);
+				}
+
+			?>
 		</div>
 	</body>
 </html>
 <?php mysqli_free_result($SacarProductos) ?>
-<?php mysqli_free_result($SacarCif) ?>
+
