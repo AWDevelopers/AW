976c85d5554dcf4dc37c12b795b4927f3ab5b8eb
mirlop01 <mirlop01@ucm.es>
2016-05-17 13:13:00 +0200

    Update tienda.php

--- proyecto/estatica/tienda.php
+++ proyecto/estatica/tienda.php
@@ -1,67 +1,58 @@
-<?php include('includes/config.php');
-	//Producto
-	$query_SacarProductos = 'SELECT * FROM producto';
-	$SacarProductos = mysqli_query($connection, $query_SacarProductos);
-	if(mysqli_num_rows($SacarProductos) > 0){
-		$row_SacarProductos = mysqli_fetch_assoc($SacarProductos);
-	}
-	
-?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Tienda - InCommOng</title>
 		<link rel="stylesheet" type="text/css" href="<?php echo $urlAbsoluta ?>css/estilos.css">
-			<link rel="stylesheet" type="text/css" href="<?php echo $urlAbsoluta ?>css/colorsandtext.css"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo $urlAbsoluta ?>css/colorsandtext.css"/>
+
 		<!--<link rel="stylesheet" type="text/css" href="css/tienda.css">-->
 	</head>
 	<body>
 		<div id='contenedor'>
 			<!--Aqui va el menu y la cabecera que es comun-->
 			<?php require 'common.php'; ?>
+		
 		</div>
 		<!--CONTENIDO-->
 
 		<div id="contenido">
-
+			
 			<div class="filtro">
 				<p>Filtros:</p>
 				<!--Estos botones deberían hacer cosas, pero sin bd no van a poder-->
 				<button>Precio</button>
 				<button>Relevancia</button>
-				<button>Ventas</button>
+				<button>Nombre</button>
 			</div>
-
 			<?php 
-				if(mysqli_num_rows($SacarProductos) > 0){
-					do {
-						$nombreOngDelProducto = $row_SacarProductos['CIFOng'];
-						$query_SacarCif = "SELECT nombre FROM ong WHERE  CIF = $nombreOngDelProducto";
-						$SacarCif = mysqli_query($connection, $query_SacarCif);
-						$row_SacarCif = mysqli_fetch_assoc($SacarCif);
-			?>
-						<div class="producto"> 
-							<h3><?php echo $row_SacarProductos['nombre'] ?></h3>
-							<a href="producto/<?php echo $row_SacarProductos['idProducto'] ?>"><img src="img/default-image.jpg"/><a>
-							<h3><?php echo $row_SacarCif['nombre'] ?></h3>
-							<h3 class="precio"> Precio: <?php echo $row_SacarProductos['precio'] ?> €</h3>
-							<form>
-								<select>
-									<?php for ($i=1; $i <= $row_SacarProductos['stock'] ; $i++) { ?>
-										<option <?php if ($i==1) echo 'selected'?>> <?php echo $i ?>
-									<?php } ?>
-								</select>
-								<input type="submit" value="Comprar">
-							</form>
-						</div>
-			<?php 	} while ($row_SacarProductos = mysqli_fetch_assoc($SacarProductos)); 
 
-					mysqli_free_result($SacarCif);
-				}
+				include  ('scriptTienda.php');
+				$nombreONG ='';
+				$id='';
+				$nombre='';
+				$precio='';
+				$stock=''	;
+
+				 mostrarTienda($nombreONG, $id, $nombre, $precio, $stock,$n ); 
 
 			?>
+			<?php for($i = 0; $i < $n; $i++){ ?>
+			<div class="producto"> 
+				
+
+					<h3><?php echo $nombre[$i] ?></h3>
+					<a href="producto/<?php echo $id[$i] ?>"><img src="img/default-image.jpg"/><a>
+					<h3><?php echo $nombreONG[$i] ?></h3>
+					<h3 class="precio"> Precio: <?php echo $precio[$i] ?>€</h3>
+					
+
+				
+			</div>
+			<?php }	?>	
 		</div>
 	</body>
 </html>
-<?php mysqli_free_result($SacarProductos) ?>
+
+
 
