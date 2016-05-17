ca9922b51b715f801e6dabd53d6e7941a6393532
mirlop01 <mirlop01@ucm.es>
2016-05-17 13:13:47 +0200

    Update vistaproducto.php

--- proyecto/estatica/vistaproducto.php
+++ proyecto/estatica/vistaproducto.php
@@ -1,34 +1,4 @@
-<?php include('includes/config.php');
-	//Consultar la url para obtener la id del producto
-	function sacarLaId($id){
-		global  $connection;
-		$query_SacarUrl = sprintf("SELECT producto.idProducto FROM producto WHERE producto.idProducto = '%s'", $id);
-		$SacarUrl = mysqli_query( $connection, $query_SacarUrl) ;
-		$row_SacarUrl = mysqli_fetch_assoc($SacarUrl);
 
-		return $row_SacarUrl['idProducto'];
-		mysqli_free_result($SacarUrl);
-	}
-
-	//La id del producto
-	$elProducto = sacarLaId($_GET['producto']);
-
-	//Ver producto selecionado
-	$query_SacarElProducto = sprintf("SELECT * FROM producto WHERE idProducto = '%s'", $elProducto,'int');
-	$SacarElProducto = mysqli_query($connection, $query_SacarElProducto) ;
-	$row_SacarElProducto = mysqli_fetch_assoc($SacarElProducto);
-	$totalRows_SacarElProducto = mysqli_num_rows($SacarElProducto);
-
-	//Cif del producto 
-	$cifProducto = $row_SacarElProducto['CIFOng'];
-
-	//Sacar el nombre del Cif
-	
-	$query_SacarNombreONG = sprintf("SELECT nombre FROM ong WHERE CIF = '%s'", $cifProducto,'int');
-	$SacarNombreONG = mysqli_query($connection, $query_SacarNombreONG);
-	$row_SacarNombreONG = mysqli_fetch_assoc($SacarNombreONG);
-	$totalRows_SacarNombreONG = mysqli_num_rows($SacarNombreONG);
-?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -38,26 +8,40 @@
 	</head>
 	<body>
 		<!--CABECERA+MENU-->
-		<<div id='contenedor'>
+		<div id='contenedor'>
 			<!--Aqui va el menu y la cabecera que es comun-->
 			<?php require 'common.php'; ?>
 		</div>
+
 		<!--CONTENIDO-->
 		<div id="contenido">
+			<?php 
+
+			include('scriptProducto.php');
+			$nombreONG ='';
+			$id='';
+			$nombre='';
+			$precio='';
+			$stock=''	;
+
+			mostrarProducto($nombreONG, $nombre, $precio, $stock, $descCorta, $descLarga ); 
+
+			?>
+
 			<div class="columnaIzda">
-				<h1><?php echo $row_SacarElProducto['nombre'] ?></h1>
-				<img src="img/default-image.jpg"/>
-				<h1><?php echo $row_SacarNombreONG['nombre'] ?></h1>
-				<h3><?php echo $row_SacarElProducto['descripcionCorta'] ?></h3>
-				<p><?php echo $row_SacarElProducto['descripcionLarga'] ?></p>	
+				<h1><?php echo $nombre ?></h1>
+				<img src="<?php echo $urlAbsoluta ?>img/default-image.jpg"/>
+				<h1><?php echo $nombreONG ?></h1>
+				<h3><?php echo $descCorta ?></h3>
+				<p><?php echo $descLarga ?></p>	
 			</div>
 			
 			<div class="columnaDcha">
-				<h3>Precio: <?php echo $row_SacarElProducto['precio'] ?> €</h3>
-				<p>Num.Unidades <?php echo $row_SacarElProducto['stock'] ?></p>
+				<h3>Precio: <?php echo $precio ?> €</h3>
+				<p>Num.Unidades <?php echo $stock?></p>
 				<form>
 					<select>
-						<?php for ($i=1; $i <= $row_SacarElProducto['stock']; $i++) { ?>
+						<?php for ($i=1; $i <= $stock; $i++) { ?>
 							<option <?php if ($i==1) echo 'selected'?>> <?php echo $i ?>
 						<?php } ?>
 					</select>
@@ -67,5 +51,5 @@
 		</div>
 	</body>
 </html>
-<?php mysqli_free_result($SacarElProducto) ?>
-<?php mysqli_free_result($SacarNombreONG) ?>
+
+
