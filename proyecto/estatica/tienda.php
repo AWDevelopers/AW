e94ca04fa8493a20dfbd963417cb27a0996a47f2
mirlop01 <mirlop01@ucm.es>
2016-05-24 10:59:50 +0200

    Update tienda.php

--- proyecto/estatica/tienda.php
+++ proyecto/estatica/tienda.php
@@ -14,46 +14,26 @@
 		
 		<!--Aqui va el contenido-->
 		<div class="contenido">
-		
-			
-			
 			<div class="filtro">
 				<p>Filtros:</p>
 				<!--Estos botones deberían hacer cosas, pero sin bd no van a poder-->
 				<button>Precio</button>
-				<button>Relevancia</button>
+				<button>Popularidad</button>
 				<button>Nombre</button>
 			</div>
 			
 			<?php 
 
 				include  ('includes/scriptTienda.php');
-				
-		
 
 				$tienda = new tienda();
 				$tienda->cargarDatosTienda();
 			?>
 
-			<?php for($i = 1; $i <= $tienda->getContador();$i++ ){ ?>
-
+			<?php for($i = 1; $i < $tienda->getContador(); $i++){ ?>
 				<div class="producto"> 
-
 					<h3><?php echo $tienda->getNombreProductos($i) ?></h3>
-				$tienda = new tienda;
-				$tienda->insertaProductosTienda();
-
-			?>
-			<?php for($i = 0; $i < $tienda->getContador(); $i++){ ?>
-			<div class="producto"> 
-				
-
-					<h3><?php echo $tienda->getNombreProductos($i) ?></h3>
-					<a href="producto/<?php echo $tienda->getIdProductos($i)  ?>"><img src="img/default-image.jpg"/><a>
-					<h3><?php echo $tienda->getNombreONGProductos($i)  ?></h3>
-					<h3 class="precio"> Precio: <?php echo $tienda->getPrecioProductos($i)  ?>€</h3>
-					
-
+			
 					<form action="vistaProducto.php" method = "post">
 					
 						<input type="hidden" name="pid" value= "<?php echo $i; ?> " />
@@ -62,8 +42,8 @@
 					
 					<h3><?php echo $tienda->getNombreONGProductos($i) ?></h3>
 					<h3 class="precio"> Precio: <?php echo $tienda->getPrecioProductos($i) ?>€</h3>
-					<button >Comprar</button>
-				
+					<button >Comprar</button>	
+					
 				
 				</div>
 			<?php } ?>
@@ -71,10 +51,3 @@
 	</div>
 </body>
 </html>
-
-
-
-
-
-
-
