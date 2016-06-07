4b230b413f18f9fc6ab23c167a0c63431461610b
Nerea <negomez@ucm.es>
2016-06-07 15:51:20 +0200

    Update registro.php

--- proyecto/estatica/includes/registro.php
+++ proyecto/estatica/includes/registro.php
@@ -19,17 +19,20 @@
     $verificar_usuario = 0;//Creamos la variable $verificar_usuario que empieza con el valor 0 y si la condición que verifica el usuario(abajo), entonces la variable toma el valor de 1 que quiere decir que ya existe ese nombre de usuario por lo tanto no se puede registrar
   	
 	while($result = mysqli_fetch_object($sql)) { 
-	    if($result->usuario == $_POST['usuario'] or $result->pass == $_POST['contraseña']){  //Esta condición verifica si ya existe el usuario 
+	    if($result->usuario == $_POST['usuario'] or $result->email == $_POST['email']){  //Esta condición verifica si ya existe el usuario 
 	        $verificar_usuario = 1; 
 	    } 
 	} 
 	  
 	if($verificar_usuario == 0) { 
-	    $nuevo_user = new Usuario($nombre,$dni,$app,$direccion,$cp,$user,$pass,$email,$fecha,$avatar,$sexo,$tlf);
-	    $nuevo_user->addUser($connection);
-
-	    header("Location: ../index.php"); 
-	    echo 'Usted se ha registrado correctamente.';
+	    $nuevo_user = new Usuario($nombre,$dni,$app,$direccion,$cp,$user,MD5($pass),$email,$fecha,$avatar,$sexo,$tlf);
+	    if($nuevo_user->validar_dni($dni)){
+			$nuevo_user->addUser($connection);
+			header("Location: ../index.php"); //Se ha registrado correctamente
+	     }
+	     else{
+			header("Location: ../registrate.php");  //Error en el dni
+	     }
 	}else{ 
 	    echo 'Este usuario ya ha sido registrado anteriormente.';
 	    header("Location: ../registrate.php"); 
