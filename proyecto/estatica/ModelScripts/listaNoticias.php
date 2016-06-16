b4c80d871dfe71da61ecf05454db1e02de262d19
Nerea <negomez@ucm.es>
2016-06-17 01:16:02 +0200

    Add files via upload

---
+++ proyecto/estatica/ModelScripts/listaNoticias.php
@@ -0,0 +1,26 @@
+<?php
+	require_once '/../DaoScripts/DaoNoticia.php';
+	class ListaNoticias{
+		
+		public function getListaNoticiasPrimarias(){
+			$dao = new DaoNoticias();
+			return ($dao->listaNoticiasPrimarias());
+		}
+		
+		public function getListaNoticiasSecundarias(){
+			$dao = new DaoNoticias();
+			return ($dao->listaNoticiasSecundarias());
+		}
+		
+		public function getListaNoticiasTerciarias(){
+			$dao = new DaoNoticias();
+			return ($dao->listaNoticiasTerciarias());
+		}
+		
+		public function getListaNoticiasOtras(){
+			$dao = new DaoNoticias();
+			return ($dao->listaNoticiasOtras());
+		}
+		
+	}
+?>
