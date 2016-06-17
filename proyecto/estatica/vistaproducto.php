f3a74ea96fc6fc2f920369cd87268780df21652b
mirlop01 <mirlop01@ucm.es>
2016-06-17 04:38:14 +0200

    Add files via upload

---
+++ proyecto/estatica/vistaproducto.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Producto - InCommOng</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css">
+		<link rel="stylesheet" type="text/css" href="css/colorsandtext.css"/>
+	</head>
+	<body>
+		<!--CABECERA+MENU-->
+		<div id='contenedor'>
+			<!--Aqui va el menu y la cabecera que es comun-->
+			<?php require 'common.php'; ?>
+		</div>
+
+		<!--CONTENIDO-->
+		<div class="contenido">
+			<?php
+					require_once "ViewScripts/ProductosVista.php";
+					muestraProducto($_GET['idProducto']);
+
+			?>
+		</div>
+	</body>
+</html>
+
+
+
+
+
+ 
