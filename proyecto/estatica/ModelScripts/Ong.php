f56e4f9837ff9c698ba21b39fc6e800423253757
MonicaHG <monicahg_93@hotmail.com>
2016-06-17 09:56:10 +0200

    Update Ong.php

--- proyecto/estatica/ModelScripts/Ong.php
+++ proyecto/estatica/ModelScripts/Ong.php
@@ -38,35 +38,6 @@
 		
 		public function getTelefono(){ return $this->telefono; }
 		
-		
-		/*---------------------- MOSTRAR Y CARGAR DATOS -------------------------------*/
-
-		public function mostrarDatosOng($mysqli, $nombreong){
-			$consulta = "SELECT nombre, direccion, email, telefono FROM ong WHERE nombre = '$nombreong'";
-			$resultado = mysqli_query($mysqli,$consulta);
-			return $resultado;
-		}
-
-		public function cargarDatosOng($cif){
-			include 'includes/config.php';
-
-			$sql = "SELECT * FROM ong WHERE CIF = '$cif'";
-			$consulta = $connection->query($sql);
-
-			if(!$consulta|| $consulta->num_rows === 0){
-				echo "Se ha producido un error con ongs, inténtalo más tarde.";
-				exit;
-			}
-			$datos = $consultaP->fetch_assoc();
-			
-			$this->cif = $datos['CIF'];
-			$this->nombre = $datos['nombre'];
-			$this->direccion = $datos['direccion'];
-			$this->email = $datos['email'];
-			$this->usuario = $datos['usuario'];
-			$this->pass = $datos['pass'];
-			$this->telefono = $datos['telefono'];
-		}
 	}
 ?>
 
