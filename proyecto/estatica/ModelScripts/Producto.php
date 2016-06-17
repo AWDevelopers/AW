6626f9b0ab9412441a42d5c689b2251e3ca24109
mirlop01 <mirlop01@ucm.es>
2016-06-17 13:47:17 +0200

    Add files via upload

---
+++ proyecto/estatica/ModelScripts/Producto.php
@@ -0,0 +1,57 @@
+<?php
+	
+	class Producto{
+		private $idProducto;
+		private $stock;
+		private $precio;
+		private $nombre;
+		private $descripcionCorta;
+		private $descripcionLarga;
+		private $nombreOng;
+		private $imagen;
+
+		function Producto($idProducto, $nombreOng, $stock, $precio, $nombre, $descCorta, $descLarga, $imagen){
+			$this->idProducto = $idProducto;
+			$this->stock = $stock;
+			$this->precio = $precio;
+			$this->nombre = $nombre;
+			$this->descripcionCorta = $descCorta;
+			$this->descripcionLarga = $descLarga;
+			$this->nombreOng = $nombreOng;
+			$this->imagen = $imagen;
+		}
+
+		public function getIdProducto(){
+			return $this->idProducto;
+		}
+
+		public function getNombreProducto(){
+			return $this->nombre;
+		}
+		public function getPrecioProducto(){
+			return $this->precio;
+		}
+		public function getstockProducto(){
+			return $this->stock;
+		}
+		public function getNombreONGProducto(){
+			return $this->nombreOng;
+		}
+		public function getDescLargaProducto(){
+			return $this->descripcionLarga;
+		}
+		public function getDescCortaProducto(){
+			return $this->descripcionCorta;
+		}
+		public function getImagen(){
+			return $this->imagen;
+		}
+
+
+		
+
+		
+	}
+
+
+?>
