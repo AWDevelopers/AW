7a38329ab322fab92e618f2b277beb1ed4b31cc0
MonicaHG <monicahg_93@hotmail.com>
2016-06-18 21:39:46 +0200

    Add files via upload

---
+++ proyecto/estatica/includes/ModelScripts/GestorDonaciones.php
@@ -0,0 +1,45 @@
+<?php
+	require_once '/../DaoScripts/DaoProyectos.php';
+	require_once '/../DaoScripts/DaoDonaciones.php';
+	class GestorDonaciones{
+
+		private $dao;
+		function __construct(){
+			$this->dao = new DaoProyectos();
+		}
+		public function getListaProyectosVoluntarios(){
+			$lista = $this->dao->listaProyectosVoluntarios();
+			$array = new ArrayObject();
+			for($i= 0; $i <sizeof($lista) ; $i++){
+			@$array->append(new Proyectos($lista[$i]['idProyecto'], $lista[$i]['CIFOng'], $lista[$i]['fechaCreacion'], $lista[$i]['dineroNecesario'], $lista[$i]['dineroAcumulado'], $lista[$i]['nombre'], $lista[$i]['descripcionCorta'], $lista[$i]['descripcionLarga'], $lista[$i]['imagen'],$lista[$i]['numVoluntarios']));
+			}
+			return $array;
+		}
+
+		public function getProyecto($id){
+			return ($this->dao->seleccionaProyecto($id));
+		}
+
+		public function nuevoProyecto($nombre,$cif,$dineroNecesario,$descripcionCorta,$descripcionLarga,$imagen, $numVoluntarios, $fechaFin, $rol){
+			$daoO = new DaoOngs();
+			$end_time   =   strtotime($fechaFin);
+			$cur_time   =   strtotime(now);
+			if($end_time > $cur_time){
+				#if($daoO->seleccionaOng($cif) != null){
+				#$app =  App::getSingleton();
+				#if($app->tieneRol($rol))
+					return ($this->dao->insertaProyecto($nombre,$cif,$dineroNecesario,$descripcionCorta,$descripcionLarga,$imagen, $numVoluntarios,$dineroAcumulado));
+				#}
+			}
+			return null;
+		}
+
+		public function addDonacion($dni, $id, $dinero){
+			$daoD = new DaoDonaciones();
+
+			
+			$daoD->addDonacion($dni, $id, $dinero);
+		}
+	}
+
+?>
---
+++ proyecto/estatica/includes/ModelScripts/GestorOngs.php
@@ -0,0 +1,50 @@
+<?php
+	require_once '/../DaoScripts/DaoOngs.php';
+	class GestorOngs{
+
+		private $daoOngs;
+
+		function __construct(){
+			
+			$this->daoOngs = new DaoOngs();
+		}
+
+
+		public function seleccionaOng($cif){
+			return ($this->daoOngs->seleccionaOng($cif));
+		}
+
+		public function modificaTelefono($nuevo, $actual){
+			$dao = new DaoOngs();
+			return ($dao->modifyTelefono($nuevo, $actual));
+		}
+
+		public function modificaNombre($nuevo, $actual){
+			$dao = new DaoOngs();
+			return ($dao->modifyNombre($nuevo, $actual));
+		}
+		public function modificaUsuario($nuevo, $actual){
+			$dao = new DaoOngs();
+			return ($dao->modifyUsuario($nuevo, $actual));
+		}
+		public function modificaPass($nuevo, $actual){
+			$dao = new DaoOngs();
+			return ($dao->modifyPass($nuevo, $actual));
+		}
+		public function modificaDir($nuevo, $actual){
+			$dao = new DaoOngs();
+			return ($dao->modifyDireccion($nuevo, $actual));
+		}
+		public function modificaMail($nuevo, $actual){
+			$dao = new DaoOngs();
+			return ($dao->modifyEmail($nuevo, $actual));
+		}
+
+		public function deleteOng($cif){
+			$dao = new DaoOngs();
+			return ($dao->deleteOng($cif));
+		}
+		
+	}
+
+?>
