e491a09be12b2bd8dbd7dc6fed32df81a323392d
SilviaLenFer <sillendi@ucm.es>
2016-06-18 21:47:39 +0200

    panelAdmin

---
+++ proyecto/estatica/includes/panelAdmin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Panel de Administracion </title>
+	<link rel = "stylesheet" type = "text/css" href="css/colorsandtext.css"/>
+		<link rel = "stylesheet" type = "text/css " href="css/estilos.css"/>
+
+
+</head>
+
+<body>
+	<div id = 'contenedor'>
+		<?php require 'common.php'; ?>
+		
+		<div class = "contenido">
+			<div id = "panelAdmin">
+				<!--<ul>-->
+					<div class="cajaAdmin"><a href="formVoluntarios.php"> <button> Voluntarios</button></div></a>
+					<div class="cajaAdmin"><a href="formProyectos.php"><button> Proyectos</button></div></a>
+					<div class="cajaAdmin"><a href="formProyectos.php"><button> Usuarios</button></div></a>
+					<div class="cajaAdmin"><a href="formProyectos.php"><button> ONGs</button></div></a>
+					<div class="cajaAdmin"><a href="formProyectos.php"><button> Productos</button></div></a>
+					
+		
+				<!--</ul>-->
+	
+			</div>
+		</div>
+
+		
+		
+</body>
+</html>
