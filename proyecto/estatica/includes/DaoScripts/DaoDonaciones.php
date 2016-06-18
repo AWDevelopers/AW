0c3123a74466407f326d5d793d4a030b6f4ac29b
MonicaHG <monicahg_93@hotmail.com>
2016-06-18 21:36:51 +0200

    ultima version

---
+++ proyecto/estatica/includes/DaoScripts/DaoDonaciones.php
@@ -0,0 +1,44 @@
+<?php
+	require_once '/../ModelScripts/donacion.php';
+	require_once '/../config.php';
+	use \AW\proyecto\estatica\includes\Aplicacion as App;
+	class DaoDonaciones{
+
+		private $array;
+
+		public function listaDonaciones(){
+			$array = new ArrayOnject();
+			$con = createConnection();
+			$consulta = "SELECT * FROM donaciones";
+			$resultado = $con->query($consulta) or die ($con->error);
+
+			if($rs != NULL){
+				while($lista = $resultado->fetch_assoc()){
+					$array->append(new Donaciones($lista['donaciones_id'], $lista['DNIUsuario'], $lista['idProyecto'], $lista['donacion']));
+				}
+				mysqli_free_result($resultado);
+				closeConection($con);
+				return $array;
+			}
+		}
+
+		public function addDonacion($DNI, $id, $dinero){
+			$app = App::getSingleton();
+    		$connection = $app->conexionBd();
+			//Añado la nueva donacion a la tabla donaciones
+			$consulta = "INSERT INTO donaciones(DNIUsuario, idProyecto, donacion) VALUES ($DNI, $id, $dinero)";
+			$connection->query($consulta) or die($connection->error);
+			//Actualizo el proyecto añadiendo la nueva donacion
+			$consulta2 = "UPDATE proyecto SET dineroAcumulado = (dineroAcumulado + $dinero) WHERE idProyecto = $id";
+			$rs->free();
+			$connection->close();
+		}
+
+		public function borraDonacion($id){
+			$con = createConnection();
+			$consulta = "DELETE FROM donaciones WHERE idProyecto = '$id'";
+			$con->query($consulta) or die ($con->error);
+			closeConecction();
+		}
+	}
+?>
---
+++ proyecto/estatica/includes/DaoScripts/DaoOngs.php
@@ -0,0 +1,123 @@
+<?php
+	
+	require_once '/../ModelScripts/ong.php';
+	require_once '/../config.php';
+	use \AW\proyecto\estatica\includes\Aplicacion as App;
+
+	class DaoOngs{
+
+		private $array;
+
+		public function listaOngs(){
+			$array = new ArrayObject();
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+
+			$consulta = "SELECT * FROM ongs";
+			$resultado = $rs = $con->query($consulta) or die ($con->error);
+
+			if($resultado != NULL){
+				while($lista = $resultado->fetch_assoc()){
+					$array->append(new Ongs($lista['CIF'], $lista['nombre'], $lista['direccion'], $lista['email'], $lista['usuario'], $lista['pass'], $lista['telefono'], $lista['imagen']));
+				}
+				mysqli_free_result($resultado);
+				$rs->free();
+			$con->close();
+				return $array;
+			}
+		}
+
+		public function addOng($ong){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$consulta = "INSERT INTO ong(CIF, nombre, direccion, email, usuario, pass, telefono) VALUES ";
+			$consulta .= "('" . $ong->getCif() . "', '" . $ong->getNombre() . "', '" . $ong->getDireccion() . "', '" . $ong->getEmail() . "', '" . $ong->getUsuario() . "', '" . $ong->getPass() . "', '" . $ong->getTelefono() . "')";
+			$rs = $con->query($consulta) or die ($con->error);
+			$rs->free();
+			$con->close();
+		}
+
+		public function deleteOng($cif){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$consulta = "DELETE FROM ong WHERE CIF='$cif'";
+			$rs = $con->query($consulta) or die ($con->error);
+			$rs->free();
+			$con->close();
+		}
+
+		public function modifyUsuario($usuario_nuevo, $usuario_actual){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql = "UPDATE ong SET usuario = '$usuario_nuevo' WHERE usuario = '$usuario_actual'";
+			$con->query($sql) or die ($con->error);
+			$con->close();
+
+		}
+
+		public function modifyCif($cif_nuevo, $cif_actual){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql = "UPDATE ong SET CIF = '$cif_nuevo' WHERE CIF = '$cif_actual'";
+			$con->query($sql) or die ($con->error);
+			$con->close();
+		}
+
+		public function modifyDireccion($dir_nueva, $dir_actual){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql = "UPDATE ong SET direccion = '$dir_nueva' WHERE direccion = '$dir_actual'";
+			$con->query($sql) or die ($con->error);
+			$con->close();
+		}
+
+		public function modifyEmail($email_nuevo, $email_actual){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql = "UPDATE ong SET email = '$email_nuevo' WHERE email = '$email_actual'";
+			$con->query($sql) or die ($con->error);
+			$con->close();
+		}
+		public function modifyNombre($nombre_nuevo, $nombre_actual){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql =  "UPDATE ong SET nombre = '$nombre_nuevo' WHERE nombre = '$nombre_actual'";
+			$con->query($sql) or die ($con->error);
+			$con->close();
+		}
+
+		public function modifyPass($pass_nueva, $pass_actual){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql = "UPDATE ong SET pass = '$pass_nueva' WHERE pass = '$pass_actual'";
+			$con->query($sql) or die ($con->error);
+			$con->close();
+		}
+
+		public function modifyTelefono($tlf_nuevo, $tlf_actual){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql = "UPDATE ong SET telefono = '$tlf_nuevo' WHERE telefono = '$tlf_actual'";
+			$con->query($sql) or die ($con->error);
+			$con->close();
+		}
+
+		public function seleccionaOng($cif){
+			$app = App::getSingleton();
+    		$con = $app->conexionBd();
+			$sql = "SELECT * FROM ong WHERE CIF = '$cif'";
+			$rs = $con->query($sql) or die($con->error);
+
+			while($lista = $rs->fetch_assoc()){
+				$resultado =  new ong($lista['CIF'], $lista['nombre'], $lista['direccion'], $lista['email'], $lista['usuario'], $lista['pass'], $lista['telefono'], $lista['imagen']);
+				$rs->free();
+				$con->close();
+				return $resultado;
+			}
+
+			
+		}
+
+		
+	}
+?>
