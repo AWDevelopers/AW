435673aa3eb7bdccdc6191a19510d6d06d9223de
Silvia Lendinez Fernandez <Silvia Lendinez Fernandez>
2016-06-20 19:15:27 +0200

    cambiando formularios

--- proyecto/estatica/nuevoProyecto.php
+++ proyecto/estatica/nuevoProyecto.php
@@ -39,22 +39,25 @@
 
 			<div class="contenido">
 					<form id="formNuevoProyecto" gaction="includes/formNuevoProyecto.php" method="POST">
-						<p>Nombre del proyecto</p>
+					<div class="contenido2">	
+						<p><h1><font color = "white">Formulario para un nuevo proyecto</font></h1></p>
+						<p><h2>Nombre del proyecto: </h2></p>
 						<input type="text" name="nombre" required></input>
-						<p>CIF Ong</p>
+						<p><h2>CIF Ong: </h2></p>
 						<input id="cif" type="text" name="cif" required></input><div id="Info"></div>
-						<p>Imagen</p>
+						<p><h2>Imagen: </h2></p>
 						<input id="file_url" type="file" name="foto">
-						<p>dinero necesario</p>
+						<p><h2>Dinero necesario: </h2></p>
 						<input type="number" name="dinero"></input>
-						<p>Voluntarios necesarios</p>
+						<p><h2>Voluntarios necesarios: </h2></p>
 						<input type="number" name="voluntarios"></input>
-						<p>descripcion corta</p>
-						<textarea name="descripcionCorta" rows="4">Introduce descripcion corta...</textarea>
-						<p>descripcion larga</p>
-						<textarea name="descripcionLarga" rows="10">Introduce descripcion larga...</textarea>
+						<p><h2>Descripcion corta: </h2></p>
+						<textarea name="descripcionCorta" rows="4" placeholder= "Descripcion corta."></textarea>
+						<p><h2>Descripcion larga: </h2></p>
+						<textarea name="descripcionLarga" rows="10" placeholder= "Descripcion larga..."></textarea>
 
-						<input type="submit" name="button" value="NUEVO">
+						<p><input type="submit" name="button" value="NUEVO"></p>
+					</div>
 					</form>
 
 		</div>
--- proyecto/estatica/vistaAniadirNoticia.php
+++ proyecto/estatica/vistaAniadirNoticia.php
@@ -12,28 +12,32 @@
 			<?php require 'common.php'; ?>
 		</div>
 		<!--CONTENIDO-->
-
+		
 			<div id="contenido">
 				<div class="formulario">
 					<form action="includes/formProcesaAniadirNoticia.php" method="POST">
-						<p>Titulo de la noticia
-						<input type="text" name="titulo" required></input></p>
-						<p>Tipo de la noticia
-						<select name="tipo">
-							<option value="primaria">Primaria</option>
-							<option value="secundaria">Secundaria</option>
-							<option value="terciaria">Terciaria</option>
-							<option value="otras">Otras</option>
-						</select></p>
-						<p>Descripción corta de la noticia
-						<input type="text" name="desCorta"></input> </p>
-						<p>Descripción larga de la noticia
-						<input type="text" name="desLarga"></input></p>
-						<p>Fecha de la noticia
-						<input type="date" name="fecha"></input></p>
-                                                <p>Nombre de la imagen
-						<input type="text" name="imagen"></input></p>
-						<p><input type="submit" name="button" value="Nueva Noticia"></p>
+						<div class="contenido2">
+							<p><h1><font color = "white">Formulario para añadir una nueva noticia</font></h1></p>
+							
+							<p><h2>Titulo de la noticia:</h2> </p>
+							<input type="text" name="titulo" required></input>
+							<p><h2>Tipo de la noticia: </h2></p>
+							<select name="tipo">
+								<option value="primaria">Primaria</option>
+								<option value="secundaria">Secundaria</option>
+								<option value="terciaria">Terciaria</option>
+								<option value="otras">Otras</option>
+							</select></p>
+							<p><h2>Descripción corta de la noticia:</h2> </p>
+							<textarea name="descripcionCorta" rows="4" placeholder= "Descripcion corta." ></textarea>
+							<p><h2>Descripción larga de la noticia: </h2> </p>
+							<textarea name="descripcionLarga" rows="10" placeholder= "Descripcion larga de la noticia..." ></textarea>			
+							<p><h2>Fecha de la noticia:</h2> </p>
+							<input type="date" name="fecha"></input>
+							<p><h2>Nombre de la imagen: </h2></p>
+							<input type="text" name="imagen"></input>
+							<p><input type="submit" name="button" value="Nueva Noticia"></p>
+						</div>
 					</form>
 					
 				</div>
