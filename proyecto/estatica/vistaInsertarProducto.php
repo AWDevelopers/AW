c41c1646bda4bb008fd61fa8c2cdfd92ff85ecc1
Usuario Local <Usuario Local>
2016-06-24 19:52:44 +0200

    cambio form insertarproducto

--- proyecto/estatica/vistaInsertarProducto.php
+++ proyecto/estatica/vistaInsertarProducto.php
@@ -13,23 +13,36 @@
 		
 		<!--Aqui va el contenido-->
 		<div class="contenido">
-			<div class="insertarprod">
+			<?php
+					use \AW\proyecto\estatica\includes\Aplicacion as App;
+					$app = App::getSingleton();
+					if ($app->usuarioLogueado()  && $app->tieneRol("Admin")) {
+			?>
+			<div class="formulario">
 				<form action="includes/formInsertarProducto.php" method="POST">
-				  <p>Nombre del producto</p>
-				  	<input type="text" name="NOMBRE" required></input>
-				  <p>Precio</p>
-				  	<input type="text" name="PRECIO"></input> 
-				  <p>Descripción corta</p>
-					<input type="text" name="DCORTA" required></input>
-				  <p>Descripción larga</p>
-					<input type="text" name="DLARGA" required></input>
-				  <p>Número de unidades</p>
-					<input type="text" name="STOCK" required></input>
-				  <p>Imagen</p>
-				  <input id="file_url" type="file" name="IMAGEN"></input>
-				  <p><input type="submit" ></p>
-				</form>
-			</div>
+				<div class="contenido2">
+					
+						<div id="formulariosTitulo"><p><h1> Formulario para insertar producto </font></h1></p></div>
+					  <p><h2>Nombre del producto</h2></p>
+					  	<input type="text" name="NOMBRE" required></input>
+					  <p><h2>Precio</h2></p>
+					  	<input type="text" name="PRECIO"></input> 
+					  <p><h2>Descripción corta</h2></p>
+						<input type="text" name="DCORTA" required></input>
+					  <p><h2>Descripción larga</h2></p>
+						<input type="text" name="DLARGA" required></input>
+					  <p><h2>Número de unidades</h2></p>
+						<input type="text" name="STOCK" required></input>
+					  <p><h2>Imagen</h2></p>
+					  <input id="file_url" type="file" name="IMAGEN"></input>
+					  <p><input type="submit" ></p>
+				</div>
+				<div>
+					</form>
+				</div>
+			<?php	
+			}
+			?>
 		</div>
 	</div>
 </body>
